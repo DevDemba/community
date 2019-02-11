86325bc4b4c8feae7c305806c695029f43d2db9d
DemsDevops <m.demba.dia@gmail.com>
2019-02-11 17:46:17 +0100

    create project laravel with register login and admin pages

---
+++ community-laravel/resources/views/forbidden.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+        <h2>
+           Error 401 : l'accès n'est pas autorisé !!
+        </h2>
+
+        <p> <b>Bien tenté!!</b>
+        <p>cette page est interdit pour toi mon ami ;)</p>
+    </body>
+</html>
