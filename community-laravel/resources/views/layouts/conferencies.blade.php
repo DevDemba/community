e5c69821a116996c6b153d97fd7f32bbca7768e7
DemsDevops <m.demba.dia@gmail.com>
2019-02-13 16:22:31 +0100

    #1 display all conferencies page

--- community-laravel/resources/views/layouts/conferencies.blade.php
+++ community-laravel/resources/views/layouts/conferencies.blade.php
@@ -22,5 +22,46 @@
 </head>
 <body>
     @include('nav')
+
+    @section('content')
+        <style>
+            .uper {
+                margin-top: 40px;
+            }
+        </style>
+        <div class="uper">
+            @if(session()->get('success'))
+                <div class="alert alert-success">
+                    {{ session()->get('success') }}
+                </div><br />
+            @endif
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <td><b>ID</b></td>
+                    <td><b>Title</b></td>
+                    <td><b>Image</b></td>
+                    <td><b>Place</b></td>
+                    <td><b>Description</b></td>
+                    <td><b>Date</b></td>
+                    <td colspan="2"><b>Action</b></td>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($conferencies as $conferency)
+                    <tr>
+                        <td>{{$conferency->id}}</td>
+                        <td>{{$conferency->title}}</td>
+                        <td> <img src="images/{{ Session::get('image') }}"></td>
+                        <td>{{$conferency->place}}</td>
+                        <td>{{$conferency->description}}</td>
+                        <td>{{$conferency->date}}</td>
+                        <td><a href="" class="btn btn-primary">Voter</a></td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    @endsection
 </body>
 </html>
