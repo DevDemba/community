b6bd8d6944c21ffae6b6500fb7073a3cd9b110a6
DemsDevops <m.demba.dia@gmail.com>
2019-02-20 00:24:29 +0100

    update layout

--- community-laravel/resources/views/layout.blade.php
+++ community-laravel/resources/views/layout.blade.php
@@ -1,20 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>CRUD</title>
+
+
+
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
         <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
     </head>
     <body>
     <header>
         @include('nav')
     </header>
-    <div class="container">
+    <div class="container" style="margin-top: 70px;">
         @yield('content')
     </div>
 
     <script src="{{ asset('js/app.js') }}" type="text/js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelectorAll('.modal');
+            var instances = M.Modal.init(elems, options);
+        });
+    </script>
     </body>
 </html>
