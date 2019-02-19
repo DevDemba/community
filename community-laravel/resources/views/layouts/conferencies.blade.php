70400fa11ac9e7d45e0b7d5955a3e3cc027cc000
DemsDevops <m.demba.dia@gmail.com>
2019-02-19 22:46:12 +0100

    update conferencies template

--- community-laravel/resources/views/layouts/conferencies.blade.php
+++ community-laravel/resources/views/layouts/conferencies.blade.php
@@ -29,14 +29,39 @@
                 <tr>
                     <td>{{$conferency->id}}</td>
                     <td>{{$conferency->title}}</td>
-                    <td> <img src="images/{{ Session::get('image') }}"></td>
+                    <td><img src="{{ Session::get('image') }}" style="width: 50px; height: 50px;" alt="{{$conferency->image}}"></td>
                     <td>{{$conferency->place}}</td>
                     <td>{{$conferency->description}}</td>
                     <td>{{$conferency->date}}</td>
+                    {{--<td><a href="{{ route('viewDetails',$conferency->id)}}" class="btn btn-primary">View</a></td>--}}
+                    <td><a href="{{ route('conferencies.show', $conferency->id) }}" class="btn btn-primary">View</a></td>
                     <td><a href="" class="btn btn-primary">Voter</a></td>
+                    <!-- Modal Trigger -->
+                    {{--<td class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</td> <!-- Modal Trigger -->--}}
+                    <td><button data-target="modal1" class=" waves-effect waves-light btn modal-trigger">Modal</button></td>
+
+                    <!-- Modal Structure -->
+                    <div id="modal1" class="modal">
+                        <div class="modal-content">
+                            <h4>Modal Header</h4>
+                            <p>A bunch of text</p>
+                        </div>
+                        <div class="modal-footer">
+                            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+                        </div>
+                    </div>
                 </tr>
             @endforeach
             </tbody>
         </table>
+            <div class="d-flex justify-content-center mt-5"> {{ $conferencies->links() }} </div>
     </div>
+    </div>
+            <script>
+
+                // document.addEventListener('DOMContentLoaded', function() {
+                //     var elems = document.querySelectorAll('.modal');
+                //     // var instances = M.Modal.init(elems, options);
+                // });
+            </script>
 @endsection
