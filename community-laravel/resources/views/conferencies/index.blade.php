61ea1cf0e669f16e0c6c82777413eab21b7caa2d
DemsDevops <m.demba.dia@gmail.com>
2019-02-21 02:09:42 +0100

    update templates

--- community-laravel/resources/views/conferencies/index.blade.php
+++ community-laravel/resources/views/conferencies/index.blade.php
@@ -29,14 +29,14 @@
                 <tr>
                     <td>{{$conferency->id}}</td>
                     <td>{{$conferency->title}}</td>
-                    <td><img style="width:50px; height: 50px;" src="{{ url('images/'.$conferency->image) }}"/></td>
+                    <td><img style="width:50px; height:50px;" src="{{ url('images/'.$conferency->image) }}"/></td>
                     <td>{{$conferency->place}}</td>
                     <td>{{$conferency->description}}</td>
                     <td>{{$conferency->date}}</td>
                     <td><a href="{{ route('conferencies.show',$conferency->id)}}" class="btn btn-primary">View</a></td>
                     <td><a href="{{ route('conferencies.edit',$conferency->id)}}" class="btn btn-primary">Edit</a></td>
                     <td>
-                        <form action="{{ route('conferencies.destroy', $conferency->id)}}" method="post">
+                        <form action="{{ route('conferencies.destroy',$conferency->id)}}" method="post">
                             @csrf
                             @method('DELETE')
                             <button class="btn btn-danger" type="submit">Delete</button>
--- community-laravel/resources/views/layouts/app.blade.php
+++ community-laravel/resources/views/layouts/app.blade.php
@@ -14,7 +14,9 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    {{--<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">--}}
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
